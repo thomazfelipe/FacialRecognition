f650bbb5cc820548d7aeb2f94c815788afc5d5a2
will-nakagawa <thomazfelipe0906@gmail.com>
2017-03-01 15:18:30 -0300

    Initial Commit

---
+++ app/src/main/java/bests/pi/facialrecognition/MainActivity.java
@@ -0,0 +1,13 @@
+package bests.pi.facialrecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
