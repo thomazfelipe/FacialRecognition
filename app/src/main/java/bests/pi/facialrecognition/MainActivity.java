2b57d3be3c567e5b74a7043a0d28d3076a1f1c88
will-nakagawa <thomazfelipe0906@gmail.com>
2017-03-01 23:24:51 -0300

    Adding Activity Splash Screen and Activity  Home Screen

--- app/src/main/java/bests/pi/facialrecognition/MainActivity.java
+++ app/src/main/java/bests/pi/facialrecognition/MainActivity.java
@@ -1,13 +1,24 @@
 package bests.pi.facialrecognition;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import bests.pi.facialrecognition.Resources.HomeScreen;
+
+public class MainActivity extends AppCompatActivity implements Runnable{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed((Runnable) this,4000);
+    }
+    @Override
+    public void run()
+    {
+            startActivity(new Intent(this, HomeScreen.class));
     }
 }
---
+++ app/src/main/java/bests/pi/facialrecognition/Resources/HomeScreen.java
@@ -0,0 +1,14 @@
+package bests.pi.facialrecognition.Resources;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import bests.pi.facialrecognition.*;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
